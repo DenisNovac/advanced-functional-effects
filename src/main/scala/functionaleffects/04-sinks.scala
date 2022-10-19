d9f16b75368f689b4de3ea30cd24cc3b30844bee
DenisNovac <ponydnk@gmail.com>
2022-10-19 11:53:34 +0400

    DAY 5: scalafmt

--- src/main/scala/functionaleffects/04-sinks.scala
+++ src/main/scala/functionaleffects/04-sinks.scala
@@ -1,14 +1,10 @@
-/**
- * In ZIO Streams, Sinks are consumers of values. Not only do they consume
- * values, but they are free to produce remainders ("leftovers"), as well as
- * terminate with values of a given type (or error).
- *
- * Sinks are the duals of streams. While streams emit values, sinks absorb
- * them. To create a complete pipeline, a stream must be connected to a
- * sink, because values need a place to go. All the run methods of Stream
- * are actually implemented by connecting streams to sinks and running the
- * complete pipeline.
- */
+/** In ZIO Streams, Sinks are consumers of values. Not only do they consume values, but they are free to produce
+  * remainders ("leftovers"), as well as terminate with values of a given type (or error).
+  *
+  * Sinks are the duals of streams. While streams emit values, sinks absorb them. To create a complete pipeline, a
+  * stream must be connected to a sink, because values need a place to go. All the run methods of Stream are actually
+  * implemented by connecting streams to sinks and running the complete pipeline.
+  */
 package advancedfunctionaleffects.sinks
 
 import zio._
@@ -21,11 +17,10 @@ object Constructors extends ZIOSpecDefault {
   def spec =
     suite("Constructors") {
 
-      /**
-       * EXERCISE
-       *
-       * Replace the call to `.runSum` by a call to `run` using `ZSink.count`.
-       */
+      /** EXERCISE
+        *
+        * Replace the call to `.runSum` by a call to `run` using `ZSink.count`.
+        */
       test("count") {
         val stream = ZStream(1, 2, 3, 4)
 
@@ -33,12 +28,10 @@ object Constructors extends ZIOSpecDefault {
           size <- stream.runSum
         } yield assertTrue(size.toInt == 4)
       } @@ ignore +
-        /**
-         * EXERCISE
-         *
-         * Replace the call to `.runCollect` by a call to `run` using
-         * `ZSink.take(2)`.
-         */
+        /** EXERCISE
+          *
+          * Replace the call to `.runCollect` by a call to `run` using `ZSink.take(2)`.
+          */
         test("take") {
           val stream = ZStream(1, 2, 3, 4)
 
@@ -46,13 +39,11 @@ object Constructors extends ZIOSpecDefault {
             two <- stream.runCollect
           } yield assertTrue(two == Chunk(1, 2))
         } @@ ignore +
-        /**
-         * EXERCISE
-         *
-         * Replace the call to `.runDrain` by a call to `run` using
-         * `ZSink.foreach`, specifying a callback that increments the
-         * ref by each value consumed by the sink.
-         */
+        /** EXERCISE
+          *
+          * Replace the call to `.runDrain` by a call to `run` using `ZSink.foreach`, specifying a callback that
+          * increments the ref by each value consumed by the sink.
+          */
         test("foreach") {
           val stream = ZStream(1, 2, 3, 4)
 
@@ -62,13 +53,11 @@ object Constructors extends ZIOSpecDefault {
             v   <- ref.get
           } yield assertTrue(v == 10)
         } @@ ignore +
-        /**
-         * EXERCISE
-         *
-         * Replace the call to `.runSum` by a call to `run` using a
-         * sink constructed with `ZSink.foldLeft` that multiplies all
-         * consumed values together (producing their product).
-         */
+        /** EXERCISE
+          *
+          * Replace the call to `.runSum` by a call to `run` using a sink constructed with `ZSink.foldLeft` that
+          * multiplies all consumed values together (producing their product).
+          */
         test("foldLeft") {
           val stream = ZStream(1, 2, 3, 4)
 
@@ -83,13 +72,11 @@ object Operators extends ZIOSpecDefault {
   def spec =
     suite("Operators") {
 
-      /**
-       * EXERCISE
-       *
-       * Use the `.map` method of the provided `sink` to map the collected
-       * values into the length of the collected values. Then replace the
-       * call to `.runCount` with a call to `.run(sink)`.
-       */
+      /** EXERCISE
+        *
+        * Use the `.map` method of the provided `sink` to map the collected values into the length of the collected
+        * values. Then replace the call to `.runCount` with a call to `.run(sink)`.
+        */
       test("map") {
         val sink = ZSink.collectAll[Int].map(_.length)
 
@@ -99,12 +86,10 @@ object Operators extends ZIOSpecDefault {
           value <- stream.runCount
         } yield assertTrue(value == 4)
       } @@ ignore +
-        /**
-         * EXERCISE
-         *
-         * Use `zipPar` to parallel zip `ZSink.count` and `ZSink.sum` together,
-         * to produce a tuple of their outputs.
-         */
+        /** EXERCISE
+          *
+          * Use `zipPar` to parallel zip `ZSink.count` and `ZSink.sum` together, to produce a tuple of their outputs.
+          */
         test("zipPar") {
           def zippedSink: ZSink[Any, Nothing, Int, Nothing, (Long, Int)] =
             ???
@@ -115,12 +100,10 @@ object Operators extends ZIOSpecDefault {
             value <- stream.run(zippedSink)
           } yield assertTrue(value == (4L, 10))
         } @@ ignore +
-        /**
-         * EXERCISE
-         *
-         * Use `zip` to zip `ZSink.take(3)` and `ZSink.collectAll[Int]` together,
-         * to produce a tuple of their outputs.
-         */
+        /** EXERCISE
+          *
+          * Use `zip` to zip `ZSink.take(3)` and `ZSink.collectAll[Int]` together, to produce a tuple of their outputs.
+          */
         test("zip") {
           def zippedSink: ZSink[Any, Nothing, Int, Int, (Chunk[Int], Chunk[Int])] =
             ???
@@ -131,12 +114,10 @@ object Operators extends ZIOSpecDefault {
             value <- stream.run(zippedSink)
           } yield assertTrue(value == (Chunk(1, 2, 3), Chunk(4)))
         } @@ ignore +
-        /**
-         * EXERCISE
-         *
-         * Use `ZSink.take` and `ZSink.collectAll` in the provided `for`
-         * comprehension to make the test pass.
-         */
+        /** EXERCISE
+          *
+          * Use `ZSink.take` and `ZSink.collectAll` in the provided `for` comprehension to make the test pass.
+          */
         test("flatMap") {
           val sink =
             for {
@@ -154,12 +135,10 @@ object Operators extends ZIOSpecDefault {
     }
 }
 
-/**
- * GRADUATION
- *
- * To graduate from this section, you will implement a sink that writes results
- * to a persistet queue.
- */
+/** GRADUATION
+  *
+  * To graduate from this section, you will implement a sink that writes results to a persistet queue.
+  */
 object Graduation extends ZIOSpecDefault {
   trait PersistentQueue[-A] {
     def append(a: A): Task[Unit]
